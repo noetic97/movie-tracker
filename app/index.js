ee4a3c1082dd2f8358cd3321521c8a2acd1cece8
Joe Heitkamp <noetic97@gmail.com>
2017-07-11 16:40:17 -0600

    more initial setup

--- app/index.js
+++ app/index.js
@@ -2,5 +2,27 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import MovieIndex from './components/movieIndex';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createHistory from 'history/createBrowserHistory'
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
+// import reducers from './reducers';
 
-ReactDOM.render(<App/>, document.getElementById('main'))
+const history = createHistory();
+const middleWare = routerMiddleware(history);
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(
+  combineReducers({
+    router: routerReducer
+  }),
+  applyMiddleware(middleWare),
+  devTools
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App/>
+    </ConnectedRouter>
+  </Provider>, document.getElementById('main'))
